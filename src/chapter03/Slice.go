c243d9a1c39c5c8c1e086ad2ec2cc1cb6aa4a4cc
LeonQiuM <qiumeng627@gmail.com>
2019-07-21 19:28:26 +0800

    slice2

--- src/chapter03/Slice.go
+++ src/chapter03/Slice.go
@@ -29,6 +29,22 @@ func main() {
 	fmt.Println(n3)      //  切片被翻转
 	fmt.Println(numbers) // 源数组的切片位置也被翻转
 
+	// 手动比较切片
+	s := []string{"abc", "xyz", "bbb", "abc", "xyz"}
+	s1 := s[0:2]
+	s2 := s[3:]
+	//  fmt.Println(s1==s2)  // 不能直接比较
+	fmt.Println(s1, s2)
+	fmt.Println(equal(s1, s2))
+
+	// 但是可以判断切片是否为空
+	fmt.Println(s1 == nil)
+
+	//  使用 make 开创建切片
+	ok1 := make([]int, 20) //  并且各个初始化为 0 值
+	ok2 := make([]string, 20)
+	fmt.Println(ok1, ok2)
+
 }
 
 func reverse(s []int) {
@@ -36,4 +52,19 @@ func reverse(s []int) {
 		s[i], s[j] = s[j], s[i]
 
 	}
+}
+
+func equal(x, y []string) bool {
+	// 两个分片的比较
+	if len(x) != len(y) {
+		return false
+	}
+	for i := range x {
+		if x[i] != y[i] {
+			return false
+		}
+	}
+
+	return true
+
 }
