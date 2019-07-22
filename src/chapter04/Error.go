380864566b76fae121fe1240cfea892baa159379
LeonQiuM <qiumeng627@gmail.com>
2019-07-22 22:52:53 +0800

    error

--- src/chapter04/Error.go
+++ src/chapter04/Error.go
@@ -1,9 +1,35 @@
 package main
 
-//
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"os"
+)
+
+//  错误处理
 /*
 
  */
-func main() {
 
+func ProcessIo() error {
+	in := bufio.NewReader(os.Stdin)
+	for {
+		r, _, err := in.ReadRune()
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			return fmt.Errorf("read faild: %v", err)
+		}
+		fmt.Print(string(r))
+	}
+	return nil
+
+}
+func main() {
+	err := ProcessIo()
+	if err != nil {
+		fmt.Print(err)
+	}
 }
