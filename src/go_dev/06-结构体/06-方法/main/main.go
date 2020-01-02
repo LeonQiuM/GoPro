1288fc0a1ab9d82b3123643075af4526018460ec
LeonQiuM <qiumeng627@gmail.com>
2020-01-02 21:20:05 +0800

    匿名方法

--- src/go_dev/06-结构体/06-方法/main/main.go
+++ src/go_dev/06-结构体/06-方法/main/main.go
@@ -9,13 +9,21 @@ type Student struct {
 	score int
 }
 
+// People docs
+type People struct {
+	float64 // 匿名字段
+	Student // 匿名字段
+}
+
 func (student *Student) study(course string) {
 	fmt.Printf("Student:%s studying %s\n", student.name, course)
 
 }
 
 func main() {
-	var stu1 Student
+	var stu1 People
 	stu1.name = "jack"
+	stu1.float64 = 33.6
+	fmt.Printf("we have %f mil people\n", stu1.float64)
 	stu1.study("math")
 }
