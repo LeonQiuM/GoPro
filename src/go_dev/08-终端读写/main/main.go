f758cb2c52e1cbaf3406916416ca909b25e1ec2b
LeonQiuM <qiumeng627@gmail.com>
2020-01-04 23:07:56 +0800

    add

--- src/go_dev/08-终端读写/main/main.go
+++ src/go_dev/08-终端读写/main/main.go
@@ -1,11 +1,24 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+	"time"
+)
 
 //
 /*
 
  */
 func main() {
-	fmt.Fprintf()
+	for {
+		time.Sleep(time.Second)
+		file, err := os.OpenFile("./log.log", os.O_CREATE|os.O_WRONLY, 0644)
+		if err != nil {
+			fmt.Println("not such file or dir")
+		}
+		defer file.Close()
+		//fmt.Fprintf(os.Stdout, "xxx\n")
+		fmt.Fprint(file, "xxxx\n")
+	}
 }
