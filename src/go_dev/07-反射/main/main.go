15b66d8910bb1e610bbe8d571752e12ba748dab8
LeonQiuM <qiumeng627@gmail.com>
2020-01-04 16:44:03 +0800

    add

--- src/go_dev/07-反射/main/main.go
+++ src/go_dev/07-反射/main/main.go
@@ -6,7 +6,8 @@ import (
 )
 
 type Student struct {
-	Name string
+	Name string `json:"student_name"`
+	Age  int    `json:"student_age"`
 }
 
 func main() {
@@ -16,6 +17,16 @@ func main() {
 	//	Name:"jack",
 	//}
 	// test(std)
+
+	xxx := 123
+	setTest(&xxx)
+	// 通过反射设置,但是无法改变值类型的副本的值，所以最好传入指针
+
+	// 通过反射操作结构体
+	var as Student = Student{
+		Name: "Alex",
+	}
+	structTest(as)
 }
 
 // 接口==类型<-->类
@@ -45,4 +56,48 @@ func test(x interface{}) {
 	//fmt.Println("get value",v.Type())
 	//fmt.Println("get value",v.Bool())
 	//fmt.Println("get value",v.Bytes())
+
+}
+
+func setTest(xx interface{}) {
+	value := reflect.ValueOf(xx)
+	// reflect.Value 通过反射来设置变量的值
+	value.Elem().SetInt(100) // 通过反射设置,但是无法改变值类型的副本的值，所以最好传入指针
+	// reflect 中无法使用指针，故提供了 Elem方法来操作指针指向的变量
+	fmt.Println(value.Elem())
+}
+
+func (student Student) Print() {
+	fmt.Println(student)
+}
+
+//func (student Student) Create(name string) {
+//	student.Name = name
+//}
+
+func structTest(as interface{}) {
+	va := reflect.ValueOf(as)
+	kd := va.Kind()
+	if kd != reflect.Struct {
+		// 常量类型不是结构体
+		fmt.Println("expect struct")
+		return
+	}
+	fieldsNum := va.NumField() // 有多少个字段
+	fmt.Println("fieldsNum", fieldsNum)
+	methodNum := va.NumMethod() // 有多少中方法
+	fmt.Println("methodNum", methodNum)
+	// 通过反射来调用结构体中的方法
+	var reflectVauleSlice []reflect.Value
+	va.Method(0).Call(reflectVauleSlice)
+	// 通过反射获取多个字段的值
+	for i := 0; i < fieldsNum; i++ {
+		fmt.Printf("index[%d],value[%v],type[%v]\n", i, va.Field(i), va.Field(i).Kind())
+	}
+	// 修改 struct 的字段名称,修改的话传入的需要是一个指针
+	// va.Elem().Field(0).SetString("James")
+	// 获取 struct 的 tag 相关,tag 是类型的一部分
+	studentNameTag := reflect.TypeOf(as).Field(1).Tag.Get("json")
+	fmt.Println(studentNameTag)
+
 }
